be40f995d2fb80833c15428c3b687a019518125a
Oksana-Dyka15888 <kseniadika2210@gmail.com>
2022-08-04 10:17:56 +0300

    add

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -3,30 +3,26 @@ const refs = {
     form: document.querySelector('.feedback-form'),
 }
 const STORAGE_KEY = 'feedback-form-state';
-
-
-refs.form.addEventListener('submit', throttle(onFormSubmit,500));
-refs.form.addEventListener('input', evt => {
-    let savedData= localStorage.getItem('STORAGE_KEY')
-    if (!savedData) {
+refs.form.addEventListener('submit',onFormSubmit);
+refs.form.addEventListener('input', throttle( evt => {
+    let savedData = localStorage.getItem('STORAGE_KEY')
+    if (savedData) {
+        savedData = JSON.parse(savedData)
+    } else {
         savedData = {}
     }
-        savedData[evt.target.name] = evt.target.value;
-    
+    savedData[evt.target.name] = evt.target.value;
     localStorage.setItem('feedback-form-state', JSON.stringify(savedData))   
-
-})
+}),500)
 function onFormSubmit(evt) {
     evt.preventDefault();
     evt.currentTarget.reset();
     localStorage.removeItem('STORAGE_KEY')
-
 }
 function initForm() {
     let savedEmail = localStorage.getItem('STORAGE_KEY')
     if (savedEmail) {
         savedEmail = JSON.parse(savedData);
-        
         for (const [name, value] of Object.entries(savedData)) {
             refs.form.elements[name].value = value;
         }
